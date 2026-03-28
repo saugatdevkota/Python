c9c6ffc9889d7fb76026241d1dd19c0da8675378
Saugat Devkota <saugatdevkota2007@gmail.com>
2026-03-29 00:02:43 +0545

    Changed code for loops

--- 2. Loops/cat.py
+++ 2. Loops/cat.py
@@ -1,4 +1,16 @@
-i = 3
-while i != 0:
+#using while loop to print "Meow" 3 times.
+i = 0
+while i < 3:
     print("Meow")
-    i -= 1
+    i += 1
+
+print("_____________________________________________")
+
+#Simply goes in list and executes as long as there are items in the list.
+for i in [0, 1, 2]:
+    print("Meow")
+    
+print("_____________________________________________")
+#Using range function to print "Meow" 3 times.
+for i in range(3):
+    print("Meow")
---
+++ 2. Loops/for.py
@@ -0,0 +1,3 @@
+#Simply goes in list and executes as long as there are items in the list.
+for i in [0, 1, 2]:
+    print("Meow")
