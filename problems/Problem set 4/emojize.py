65b6a2fc9128d54bc8c3d3eaddcf0a74998fda98
Saugat Devkota <saugatdevkota2007@gmail.com>
2026-08-11 22:54:33 +0545

    Emojizer.py

---
+++ problems/Problem set 4/emojize.py
@@ -0,0 +1,11 @@
+import emoji
+def main():
+    output = input_emoji()
+    print(f"Output: {output}")
+
+def input_emoji():
+    user_input = input("Input: ")
+    return emoji.emojize(user_input)
+
+if __name__ == "__main__":
+    main()
